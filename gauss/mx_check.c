21d63ecec132ba5160c295ff066c1eb5e66bb96f
Bobur <bobs4462@gmail.com>
2018-04-06 03:03:15 +0300

    small bug fixes

--- gauss/mx_check.c
+++ gauss/mx_check.c
@@ -4,17 +4,19 @@ void mx_check(SLE * emp, int rc)
 {
 	double tmp = 0;
 	if (emp->mx[rc * emp->sz + rc] == 0) {
-		for (int i = rc + 1; i < emp->sz - 1; i++)
+		printf("reshuffling");
+		for (int i = rc + 1; i < emp->sz - 1; i++) {
 			if (emp->mx[i * emp->sz + rc] != 0) {
 				for (int j = rc; j < emp->sz; j++) {
-					tmp = emp->mx[rc * emp->sz + rc];
-					emp->mx[rc * emp->sz + rc] = emp->mx[i * emp->sz + rc];
-				 	emp->mx[i * emp->sz + rc] = tmp;
+					tmp = emp->mx[rc * emp->sz + j];
+					emp->mx[rc * emp->sz + j] = emp->mx[i * emp->sz + j];
+				 	emp->mx[i * emp->sz + j] = tmp;
 				}
 				return;
 			}
 			else 
 				continue;
+		}
 	}
 				
 }
--- gauss/processf.c
+++ gauss/processf.c
@@ -1,31 +1,32 @@
 #include "gauss.h"
 #include <ctype.h>
-static bool disposer(FILE * efp);
+static bool disposer(FILE * efp, bool * nl);
 
 
 SLE * processf(FILE * efp)
 {
 	double dtmp = 0;
+	bool nl = true;
 	unsigned short size = 0;
 	int ctmp = 0;
 	bool minus = false;
-	while ((ctmp = getc(efp)) != '\n') {
-		ungetc(ctmp, efp);
+	while (nl) {
+		disposer(efp, &nl);
 		if (fscanf(efp, "%lf", &dtmp))
 			size++;
-		else
-			disposer(efp);
+		printf("size is %d\n", size);
 	}
 	SLE * emp = (SLE *) malloc(offsetof(SLE, mx) + (size-1) * size * sizeof(double));
 	emp->sz = size; 
 	fseek(efp, 0L, SEEK_SET);
 	for (int i = 0; i < (size - 1); i++) 
 		for (int j = 0; j < size; j++) {
-			minus = disposer(efp);	
+			minus = disposer(efp, &nl);	
 			fscanf(efp, "%lf", &(emp->mx[i * size + j]));
 			if (minus)
 				emp->mx[i * size + j] *= -1;
-		}
+		} 
+	printf("%d\n", size);
 	for (int i = 0; i < (size - 1); i++) {
 		for (int j = 0; j < size; j++) {
 			printf("%f\t", emp->mx[i * size + j]);
@@ -36,13 +37,15 @@ SLE * processf(FILE * efp)
 	return emp;
 }
 
-static bool disposer(FILE * efp)
+static bool disposer(FILE * efp, bool * nl)
 {
 	int temp = 0;
 	bool minus = false;
 	while (!isdigit(temp = getc(efp)) && temp != EOF) {
 		if (temp == '-')
 			minus = true;
+		else if (temp == '\n')
+			*nl = false;
 	}
 	if (temp == EOF) {
 		printf("Файл не содержит подходящего уравнения!\n");
